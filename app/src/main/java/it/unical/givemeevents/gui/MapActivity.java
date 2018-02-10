11c888ac933cb34a704adbab0830c318eaee641a
Dorianc4 <36304293+Dorianc4@users.noreply.github.com>
2018-02-10 13:37:53 +0100

    Esta creado el mapa, hay que hacer la parte donde se llama para probarlo

--- app/src/main/java/it/unical/givemeevents/gui/MapActivity.java
+++ app/src/main/java/it/unical/givemeevents/gui/MapActivity.java
@@ -1,12 +1,21 @@
 package it.unical.givemeevents.gui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import it.unical.givemeevents.R;
 
@@ -15,16 +24,43 @@ import it.unical.givemeevents.R;
  */
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private static final String TAG = "MapActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 16f;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
+            new LatLng(-40,-168), new LatLng(71, 136));
+
+
+    //Attributes
+    private Boolean mLocationPermissionGRanted = false;
+    private GoogleMap myMap;
+
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         myMap = googleMap;
+        if (mLocationPermissionGRanted) {
+            //getDeviceLocation();
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        myMap.setMyLocationEnabled(true);
+        myMap.getUiSettings().setMyLocationButtonEnabled(false);
     }
-    private static final String TAG = "MapActivity";
-    private GoogleMap myMap;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        getLocationPermission();
     }
 
     private void initMap(){
@@ -34,5 +70,55 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     }
 
+    private void moveCamera(LatLng latlng, float zoom, String title){
+        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
+        if(title != "Current Location"){
+            MarkerOptions options = new MarkerOptions()
+                    .position(latlng)
+                    .title(title);
+            myMap.addMarker(options);
+        }
 
+    }
+
+    private void getLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionGRanted=true;
+                initMap();
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGRanted = false;
+
+        switch (requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length > 0){
+                    for (int i =0; i < grantResults.length; i++){
+                        if(grantResults[i]!= PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionGRanted =false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGRanted = true;
+                    //initialize our map
+                    initMap();
+                }
+            }
+        }
+    }
 }
