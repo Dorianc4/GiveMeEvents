27cd21d878218e0a07c431461bea184e7f05627b
maborroto <maborroto1987@gmail.com>
2018-02-13 14:37:40 -0500

    Adicionar eventos a calendario

--- app/src/main/java/it/unical/givemeevents/EventDetails.java
+++ app/src/main/java/it/unical/givemeevents/EventDetails.java
@@ -1,15 +1,30 @@
 package it.unical.givemeevents;
 
+import android.content.AsyncQueryHandler;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Message;
+import android.provider.CalendarContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import it.unical.givemeevents.model.FacebookEvent;
+import it.unical.givemeevents.util.GiveMeEventUtils;
 
 public class EventDetails extends AppCompatActivity {
+
     private FacebookEvent event;
 
     @Override
@@ -19,7 +34,7 @@ public class EventDetails extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         event = getIntent().getParcelableExtra("Event");
-       int i =0;
+        int i = 0;
     }
 
 }
