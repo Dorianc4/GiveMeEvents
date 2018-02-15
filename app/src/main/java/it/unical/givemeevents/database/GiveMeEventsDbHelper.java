e3a51b4a054ecf3e606e8e62d1910a80e309f505
Dorianc4 <36304293+Dorianc4@users.noreply.github.com>
2018-02-15 04:27:57 +0100

    Autocomplete Busqueda

--- app/src/main/java/it/unical/givemeevents/database/GiveMeEventsDbHelper.java
+++ app/src/main/java/it/unical/givemeevents/database/GiveMeEventsDbHelper.java
@@ -18,7 +18,7 @@ public class GiveMeEventsDbHelper extends SQLiteOpenHelper {
     private static final String COMMA_SEP = ",";
     static final String CATEGORY_TRACE_TABLE_NAME = "category_trace";
     static final String COLUMN_NAME_ID_CATEGORY = "id_category";
-
+    //Definicion de la tabla Lugares favoritos
     private static final String SQL_CREATE_TABLE_FAVPLACE = "CREATE TABLE IF NOT EXISTS " +
             PlaceDbContract.PlaceEntry.TABLE_NAME + " (" +
             PlaceDbContract.PlaceEntry.COLUMN_NAME_ID + " LARGEINT PRIMARY KEY," +
@@ -29,21 +29,22 @@ public class GiveMeEventsDbHelper extends SQLiteOpenHelper {
             PlaceDbContract.PlaceEntry.COLUMN_NAME_LONGITUDE + DOUBLE_TYPE + COMMA_SEP +
             PlaceDbContract.PlaceEntry.COLUMN_NAME_STREET + TEXT_TYPE + COMMA_SEP +
             PlaceDbContract.PlaceEntry.COLUMN_NAME_PICTURE + TEXT_TYPE + " ); ";
-
+    //Definicion de la tabla Traza de los Eventos
     private static final String SQL_CREATE_TABLE_EVENT_TRACE = "CREATE TABLE IF NOT EXISTS " +
             TraceDbContract.TraceEntry.TABLE_NAME + " (" +
             TraceDbContract.TraceEntry.COLUMN_NAME_ID_PLACE + TEXT_TYPE + " NOT NULL," +
             TraceDbContract.TraceEntry.COLUMN_NAME_START_TIME + TEXT_TYPE + " NOT NULL ); ";
-
+    //Definicion de la tabla Traza de las Categorias
     private static final String SQL_CREATE_TABLE_CATEGORY_TRACE = "CREATE TABLE IF NOT EXISTS " +
             CATEGORY_TRACE_TABLE_NAME + " (" +
             COLUMN_NAME_ID_CATEGORY + TEXT_TYPE + " PRIMARY KEY);";
 
-
+    //Constructor
     public GiveMeEventsDbHelper(Context context) {
         super(context, context.getString(R.string.db_name), null, 1);
     }
 
+    //Crea la BD, con tres tablas
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(SQL_CREATE_TABLE_FAVPLACE);
